ed211e08f79aeaaca1cf63919c3f45b20eab0a76
DongNguyenVanQuyen <dongnguyenvanquyen@gmail.com>
2026-07-13 21:28:25 +0700

    log connect mailer to env

--- src/configs/mailer.js
+++ src/configs/mailer.js
@@ -1,9 +1,13 @@
 // src/configs/mailer.js
 const nodemailer = require("nodemailer");
 
+console.log("MAIL_HOST:", process.env.MAIL_HOST);
+console.log("MAIL_PORT:", process.env.MAIL_PORT);
+console.log("MAIL_USER:", process.env.MAIL_USER);
+
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: 587,
+  port: process.env.MAIL_PORT,
   secure: false,
   requireTLS: true,
   auth: {
