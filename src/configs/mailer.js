71fb716a3d612f02f10b341f7a734efd9b814dce
DongNguyenVanQuyen <dongnguyenvanquyen@gmail.com>
2026-07-13 22:13:02 +0700

    new mailler use brevo

--- src/configs/mailer.js
+++ src/configs/mailer.js
@@ -1,6 +1,10 @@
 // src/configs/mailer.js
 const SibApiV3Sdk = require("@getbrevo/brevo");
 
+console.log(
+  "BREVO API Key:",
+  process.env.BREVO_API_KEY ? "Loaded" : "Not Loaded",
+);
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 apiInstance.setApiKey(
   SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
