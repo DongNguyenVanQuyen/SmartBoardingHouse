9be40d787af4be8431cd654ea48da27e3228ae8e
DongNguyenVanQuyen <dongnguyenvanquyen@gmail.com>
2026-07-15 09:32:11 +0700

    test new month

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -10,7 +10,7 @@ const { success, error: sendError } = require("../utils/response");
 const getDashboard = async (req, res) => {
   try {
     const now = new Date();
-    const currentMonth = now.getMonth() + 1;
+    const currentMonth = now.getMonth() + 6;
     const currentYear = now.getFullYear();
 
     // 1. Lấy contract trước
