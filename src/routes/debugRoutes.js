1c922947683f2da3a91c7d9032c8db529c66e9fc
DongNguyenVanQuyen <dongnguyenvanquyen@gmail.com>
2026-07-19 17:17:21 +0700

    add debug invoice month

--- src/routes/debugRoutes.js
+++ src/routes/debugRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middlewares/auth"); // đổi tên cho khớp middleware xác thực bạn đang có
+const { protect } = require("../middlewares/auth");
 const { clearMonthData } = require("../controllers/debugController");
 
 router.post("/clear-month", protect, clearMonthData);
